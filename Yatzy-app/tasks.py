998b2e7e5d46900912554fbaf76a0a52106780c1
JVS23 <juhosiren23@gmail.com>
2021-11-23 05:34:06 +0200

    invoke_coverage

--- Yatzy-app/tasks.py
+++ Yatzy-app/tasks.py
@@ -14,5 +14,12 @@ def start(ctx):
 def test(ctx):
     ctx.run("pytest src")
 
-# @task
-# def coverage-report(ctx):
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+
+@task
+def coverage_report(ctx):
+    ctx.run("coverage html")
