91962ccccb484232ddbf4d00a75b6ae364520067
JVS23 <juhosiren23@gmail.com>
2021-11-28 16:42:36 +0200

    More scorelogic

--- Yatzy-app/src/main.py
+++ Yatzy-app/src/main.py
@@ -17,7 +17,7 @@ def main():
     # fixthis
     test_set = dice_set1.print()
 
-    gained_score = player_score.check_for_fives(test_set)
+    gained_score = player_score.three_of_a_kind(test_set)
     player.add_score(gained_score)
     #
 
--- Yatzy-app/src/scorelogic.py
+++ Yatzy-app/src/scorelogic.py
@@ -8,42 +8,49 @@ class Categories:
 
     def check_for_aces(self, dice_list):
 
-        print("Checking for aces..")
         score = dice_list.count(1)
         print("Your score for Aces is: ", score)
         return score
 
     def check_for_twos(self, dice_list):
 
-        print("Checking for Twos..")
         score = dice_list.count(2) * 2
         print("Your score for Twos is: ", score)
         return score
 
     def check_for_threes(self, dice_list):
 
-        print("Checking for Threes..")
         score = dice_list.count(3) * 3
         print("Your score for Threes is: ", score)
         return score
 
     def check_for_fours(self, dice_list):
 
-        print("Checking for Fours..")
         score = dice_list.count(4) * 4
         print("Your score for Fours is: ", score)
         return score
 
     def check_for_fives(self, dice_list):
 
-        print("Checking for Fives..")
         score = dice_list.count(5) * 5
         print("Your score for Fives is: ", score)
         return score
 
     def check_for_sixes(self, dice_list):
 
-        print("Checking for Sixes..")
         score = dice_list.count(6) * 6
         print("Your score for Sixes is: ", score)
+        return score
+
+    def three_of_a_kind(self, dice_list):
+
+        score = 0
+
+        for i in dice_list:
+            if dice_list.count(i) == 3:
+                score = i * 3
+                print("Your score for Three of a kind is: ", score)
+                return score
+
+        print("Your score for Three of a kind is: ", score)
         return score
